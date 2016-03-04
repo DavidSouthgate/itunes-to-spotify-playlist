54380c1ff6680ab2a23c94a20a2b32118b44ffab
David Southgate <dav@davidsouthgate.co.uk>
2016-03-04 20:22:36 +0000

    Program will read in an itunes playlist and output a spotify playlist.

--- iTunesToSpotifyPlaylist/frmMain.Designer.cs
+++ iTunesToSpotifyPlaylist/frmMain.Designer.cs
@@ -28,8 +28,6 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.comboPlaylists = new System.Windows.Forms.ComboBox();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.dataSet1 = new System.Data.DataSet();
             this.dataTable1 = new System.Data.DataTable();
             this.dataColumn1 = new System.Data.DataColumn();
@@ -37,33 +35,21 @@
             this.dataColumn3 = new System.Data.DataColumn();
             this.dataColumn4 = new System.Data.DataColumn();
             this.dataColumn5 = new System.Data.DataColumn();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panelPlaylists = new System.Windows.Forms.Panel();
+            this.cmdGetSpotifyPlaylist = new System.Windows.Forms.Button();
+            this.dataPlaylistGridView = new System.Windows.Forms.DataGridView();
+            this.comboPlaylists = new System.Windows.Forms.ComboBox();
+            this.bwGetITunesPlaylist = new System.ComponentModel.BackgroundWorker();
+            this.dataColumn6 = new System.Data.DataColumn();
+            this.panelSpotifyPlaylist = new System.Windows.Forms.Panel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lblStatus = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataTable1)).BeginInit();
+            this.panelPlaylists.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataPlaylistGridView)).BeginInit();
+            this.panelSpotifyPlaylist.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // comboPlaylists
-            // 
-            this.comboPlaylists.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.comboPlaylists.FormattingEnabled = true;
-            this.comboPlaylists.Location = new System.Drawing.Point(13, 13);
-            this.comboPlaylists.Name = "comboPlaylists";
-            this.comboPlaylists.Size = new System.Drawing.Size(583, 21);
-            this.comboPlaylists.TabIndex = 0;
-            this.comboPlaylists.Text = "Playlists";
-            this.comboPlaylists.SelectedIndexChanged += new System.EventHandler(this.comboPlaylists_SelectedIndexChanged);
-            // 
-            // dataGridView1
-            // 
-            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(13, 41);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(583, 296);
-            this.dataGridView1.TabIndex = 1;
             // 
             // dataSet1
             // 
@@ -78,7 +64,8 @@
             this.dataColumn2,
             this.dataColumn3,
             this.dataColumn4,
-            this.dataColumn5});
+            this.dataColumn5,
+            this.dataColumn6});
             this.dataTable1.TableName = "Table1";
             // 
             // dataColumn1
@@ -105,27 +92,96 @@
             // 
             this.dataColumn5.ColumnName = "FileLocation";
             // 
+            // panelPlaylists
+            // 
+            this.panelPlaylists.Controls.Add(this.cmdGetSpotifyPlaylist);
+            this.panelPlaylists.Controls.Add(this.dataPlaylistGridView);
+            this.panelPlaylists.Controls.Add(this.comboPlaylists);
+            this.panelPlaylists.Location = new System.Drawing.Point(62, 46);
+            this.panelPlaylists.Name = "panelPlaylists";
+            this.panelPlaylists.Size = new System.Drawing.Size(639, 316);
+            this.panelPlaylists.TabIndex = 4;
+            // 
+            // cmdGetSpotifyPlaylist
+            // 
+            this.cmdGetSpotifyPlaylist.Location = new System.Drawing.Point(4, 290);
+            this.cmdGetSpotifyPlaylist.Name = "cmdGetSpotifyPlaylist";
+            this.cmdGetSpotifyPlaylist.Size = new System.Drawing.Size(632, 23);
+            this.cmdGetSpotifyPlaylist.TabIndex = 2;
+            this.cmdGetSpotifyPlaylist.Text = "Get Spotify Playlist";
+            this.cmdGetSpotifyPlaylist.UseVisualStyleBackColor = true;
+            this.cmdGetSpotifyPlaylist.Click += new System.EventHandler(this.cmdGetSpotifyPlaylist_Click);
+            // 
+            // dataPlaylistGridView
+            // 
+            this.dataPlaylistGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataPlaylistGridView.Location = new System.Drawing.Point(4, 30);
+            this.dataPlaylistGridView.Name = "dataPlaylistGridView";
+            this.dataPlaylistGridView.Size = new System.Drawing.Size(632, 254);
+            this.dataPlaylistGridView.TabIndex = 1;
+            // 
+            // comboPlaylists
+            // 
+            this.comboPlaylists.FormattingEnabled = true;
+            this.comboPlaylists.Location = new System.Drawing.Point(3, 3);
+            this.comboPlaylists.Name = "comboPlaylists";
+            this.comboPlaylists.Size = new System.Drawing.Size(633, 21);
+            this.comboPlaylists.TabIndex = 0;
+            this.comboPlaylists.Text = "Playlists";
+            this.comboPlaylists.SelectedIndexChanged += new System.EventHandler(this.comboPlaylists_SelectedIndexChanged);
+            // 
+            // dataColumn6
+            // 
+            this.dataColumn6.ColumnName = "Spotify ID";
+            // 
+            // panelSpotifyPlaylist
+            // 
+            this.panelSpotifyPlaylist.Controls.Add(this.lblStatus);
+            this.panelSpotifyPlaylist.Controls.Add(this.textBox1);
+            this.panelSpotifyPlaylist.Location = new System.Drawing.Point(12, 12);
+            this.panelSpotifyPlaylist.Name = "panelSpotifyPlaylist";
+            this.panelSpotifyPlaylist.Size = new System.Drawing.Size(599, 257);
+            this.panelSpotifyPlaylist.TabIndex = 5;
+            this.panelSpotifyPlaylist.Visible = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(3, 20);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(66, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(4, 4);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(47, 13);
+            this.lblStatus.TabIndex = 1;
+            this.lblStatus.Text = "lblStatus";
+            // 
             // frmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(608, 349);
-            this.Controls.Add(this.dataGridView1);
-            this.Controls.Add(this.comboPlaylists);
+            this.ClientSize = new System.Drawing.Size(713, 380);
+            this.Controls.Add(this.panelSpotifyPlaylist);
+            this.Controls.Add(this.panelPlaylists);
             this.Name = "frmMain";
             this.Text = "iTunes to Spotify Playlist";
             this.Load += new System.EventHandler(this.frmMain_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.Resize += new System.EventHandler(this.frmMain_Resize);
             ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataTable1)).EndInit();
+            this.panelPlaylists.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataPlaylistGridView)).EndInit();
+            this.panelSpotifyPlaylist.ResumeLayout(false);
+            this.panelSpotifyPlaylist.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.ComboBox comboPlaylists;
-        private System.Windows.Forms.DataGridView dataGridView1;
         private System.Data.DataSet dataSet1;
         private System.Data.DataTable dataTable1;
         private System.Data.DataColumn dataColumn1;
@@ -133,6 +189,15 @@
         private System.Data.DataColumn dataColumn3;
         private System.Data.DataColumn dataColumn4;
         private System.Data.DataColumn dataColumn5;
+        private System.Windows.Forms.Panel panelPlaylists;
+        private System.Windows.Forms.DataGridView dataPlaylistGridView;
+        private System.Windows.Forms.ComboBox comboPlaylists;
+        private System.Windows.Forms.Button cmdGetSpotifyPlaylist;
+        private System.ComponentModel.BackgroundWorker bwGetITunesPlaylist;
+        private System.Data.DataColumn dataColumn6;
+        private System.Windows.Forms.Panel panelSpotifyPlaylist;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lblStatus;
     }
 }
 
--- iTunesToSpotifyPlaylist/frmMain.cs
+++ iTunesToSpotifyPlaylist/frmMain.cs
@@ -11,12 +11,29 @@ using System.Windows.Forms;
 using iTunesLib;
 using System.Diagnostics;
 using System.IO;
+using Newtonsoft.Json;
+using System.Net;
 
 namespace iTunesToSpotifyPlaylist
 {
     public partial class frmMain : Form
     {
         iTunesApp itunes = new iTunesLib.iTunesApp();
+
+        public class spotify_api
+        {
+            public spotify_api_tracks tracks { get; set; }
+        }
+
+        public class spotify_api_tracks
+        {
+            public List<spotify_api_tracks_items> items { get; set; }
+        }
+
+        public class spotify_api_tracks_items
+        {
+            public string id { get; set; }
+        }
 
         public frmMain()
         {
@@ -25,6 +42,7 @@ namespace iTunesToSpotifyPlaylist
 
         private void frmMain_Load(object sender, EventArgs e)
         {
+            
             //For each playlist in iTunes
             foreach (IITPlaylist pl in itunes.LibrarySource.Playlists)
             {
@@ -32,15 +50,18 @@ namespace iTunesToSpotifyPlaylist
                 //Add playlist name to combo box
                 comboPlaylists.Items.Add(pl.Name);
             }
-
+            
             //Load entire library playlist into variable
-            IITLibraryPlaylist mainLibrary = itunes.LibraryPlaylist;
+            //IITLibraryPlaylist mainLibrary = itunes.LibraryPlaylist;
 
             //Display the entire library
-            GetTracks(mainLibrary);
+            //GetTracks(mainLibrary);
 
             //Tells the datagrid where to get data from
-            dataGridView1.DataSource = dataTable1;
+            dataPlaylistGridView.DataSource = dataTable1;
+
+            //Position UI elements
+            ui();
         }
 
         private void GetTracks(IITPlaylist playlist)
@@ -57,7 +78,6 @@ namespace iTunesToSpotifyPlaylist
                 drnew["song name"] = currTrack.Name;
                 drnew["album"] = currTrack.Album;
                 drnew["genre"] = currTrack.Genre;
-
 
                 if (currTrack.Kind == ITTrackKind.ITTrackKindFile)
                 {
@@ -80,6 +100,74 @@ namespace iTunesToSpotifyPlaylist
             //lbl_numsongs.Text = "number of songs: " + dataTable1.Rows.Count.ToString() + ", total file size: " + (totalfilesize / 1024.00 / 1024.00).ToString("#,### mb");
         }
 
+
+
+
+
+
+        private string get_spotify_track_id(string song, string artist)
+        {
+
+            //Generate a safe query to use with the spotify api
+            string query = WebUtility.UrlEncode(artist + " " + song);
+
+            //Create a new web client to contact spotify api
+            WebClient client = new WebClient();
+
+            //Get json response from spotify api
+            string api_json = client.DownloadString("https://api.spotify.com/v1/search?q=" + query + "&type=track&limit=1");
+
+            spotify_api api = JsonConvert.DeserializeObject<spotify_api>(api_json);
+
+            return api.tracks.items[0].id;
+        }
+
+
+
+
+        private void ui()
+        {
+            //
+            //panelPlaylists
+            //
+            panelPlaylists.Top = 0;
+            panelPlaylists.Left = 0;
+            panelPlaylists.Width = this.ClientSize.Width;
+            panelPlaylists.Height = this.ClientSize.Height;
+            //
+            //comboPlaylists
+            //
+            comboPlaylists.Top = 14;
+            comboPlaylists.Left = 14;
+            comboPlaylists.Width = panelPlaylists.Width - 14 - 14;
+            //
+            //dataPlaylistGridView
+            //
+            dataPlaylistGridView.Top = comboPlaylists.Top + comboPlaylists.Height + 6;
+            dataPlaylistGridView.Left = 14;
+            dataPlaylistGridView.Width = panelPlaylists.Width - 14 - 14;
+            dataPlaylistGridView.Height = this.ClientSize.Height - dataPlaylistGridView.Top - 14 - cmdGetSpotifyPlaylist.Height;
+            //
+            //cmdGetSpotifyPlaylist
+            //
+            cmdGetSpotifyPlaylist.Top = dataPlaylistGridView.Top + dataPlaylistGridView.Height + 6;
+            cmdGetSpotifyPlaylist.Left = 14;
+            cmdGetSpotifyPlaylist.Width = panelPlaylists.Width - 14 - 14;
+
+            //
+            //panelSpotifyPlaylist
+            //
+            panelSpotifyPlaylist.Top = 0;
+            panelSpotifyPlaylist.Left = 0;
+            panelSpotifyPlaylist.Width = this.ClientSize.Width;
+            panelSpotifyPlaylist.Height = this.ClientSize.Height;
+        }
+
+        private void frmMain_Resize(object sender, EventArgs e)
+        {
+            ui();
+        }
+
         private void comboPlaylists_SelectedIndexChanged(object sender, EventArgs e)
         {
             string playlist = comboPlaylists.SelectedItem.ToString();
@@ -91,6 +179,37 @@ namespace iTunesToSpotifyPlaylist
                     break;
                 }
             }
+        }
+
+        private void cmdGetSpotifyPlaylist_Click(object sender, EventArgs e)
+        {
+
+            int success_count = 0;
+            int failure_count = 0;
+            string playlist_text = "";
+
+            foreach (DataRow row in dataTable1.Rows)
+            {
+                try
+                {
+                    playlist_text = playlist_text + "spotify:track:" + get_spotify_track_id(row["song name"].ToString(), row["artist"].ToString()) + " ";
+                    success_count++;
+                }
+                 catch
+                {
+                    failure_count++;
+                }
+            }
+
+            lblStatus.Text = success_count + " tracks were converted to a spotify playlist, " + failure_count + " failed";
+
+            if(playlist_text != "")
+            {
+                Clipboard.SetText(playlist_text);
+            }
+
+            panelPlaylists.Visible = false;
+            panelSpotifyPlaylist.Visible = true;
         }
     }
 }
