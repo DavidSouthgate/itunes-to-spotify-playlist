140279268b9554d0e1ab95a7550be56b1ef01fd8
David Southgate <dav@davidsouthgate.co.uk>
2016-03-04 17:24:05 +0000

    Can load itunes playlists

--- iTunesToSpotifyPlaylist/frmMain.Designer.cs
+++ iTunesToSpotifyPlaylist/frmMain.Designer.cs
@@ -28,20 +28,111 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.comboPlaylists = new System.Windows.Forms.ComboBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataSet1 = new System.Data.DataSet();
+            this.dataTable1 = new System.Data.DataTable();
+            this.dataColumn1 = new System.Data.DataColumn();
+            this.dataColumn2 = new System.Data.DataColumn();
+            this.dataColumn3 = new System.Data.DataColumn();
+            this.dataColumn4 = new System.Data.DataColumn();
+            this.dataColumn5 = new System.Data.DataColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataTable1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // comboPlaylists
+            // 
+            this.comboPlaylists.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.comboPlaylists.FormattingEnabled = true;
+            this.comboPlaylists.Location = new System.Drawing.Point(13, 13);
+            this.comboPlaylists.Name = "comboPlaylists";
+            this.comboPlaylists.Size = new System.Drawing.Size(583, 21);
+            this.comboPlaylists.TabIndex = 0;
+            this.comboPlaylists.Text = "Playlists";
+            this.comboPlaylists.SelectedIndexChanged += new System.EventHandler(this.comboPlaylists_SelectedIndexChanged);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(13, 41);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(583, 296);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // dataSet1
+            // 
+            this.dataSet1.DataSetName = "NewDataSet";
+            this.dataSet1.Tables.AddRange(new System.Data.DataTable[] {
+            this.dataTable1});
+            // 
+            // dataTable1
+            // 
+            this.dataTable1.Columns.AddRange(new System.Data.DataColumn[] {
+            this.dataColumn1,
+            this.dataColumn2,
+            this.dataColumn3,
+            this.dataColumn4,
+            this.dataColumn5});
+            this.dataTable1.TableName = "Table1";
+            // 
+            // dataColumn1
+            // 
+            this.dataColumn1.Caption = "Song Name";
+            this.dataColumn1.ColumnName = "Song Name";
+            // 
+            // dataColumn2
+            // 
+            this.dataColumn2.Caption = "Artist";
+            this.dataColumn2.ColumnName = "Artist";
+            // 
+            // dataColumn3
+            // 
+            this.dataColumn3.Caption = "Album";
+            this.dataColumn3.ColumnName = "Album";
+            // 
+            // dataColumn4
+            // 
+            this.dataColumn4.Caption = "Genre";
+            this.dataColumn4.ColumnName = "Genre";
+            // 
+            // dataColumn5
+            // 
+            this.dataColumn5.ColumnName = "FileLocation";
             // 
             // frmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(608, 349);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.comboPlaylists);
             this.Name = "frmMain";
-            this.Text = "Form1";
+            this.Text = "iTunes to Spotify Playlist";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataTable1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox comboPlaylists;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Data.DataSet dataSet1;
+        private System.Data.DataTable dataTable1;
+        private System.Data.DataColumn dataColumn1;
+        private System.Data.DataColumn dataColumn2;
+        private System.Data.DataColumn dataColumn3;
+        private System.Data.DataColumn dataColumn4;
+        private System.Data.DataColumn dataColumn5;
     }
 }
 
--- iTunesToSpotifyPlaylist/frmMain.cs
+++ iTunesToSpotifyPlaylist/frmMain.cs
@@ -8,13 +8,89 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using iTunesLib;
+using System.Diagnostics;
+using System.IO;
+
 namespace iTunesToSpotifyPlaylist
 {
     public partial class frmMain : Form
     {
+        iTunesApp itunes = new iTunesLib.iTunesApp();
+
         public frmMain()
         {
             InitializeComponent();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            //For each playlist in iTunes
+            foreach (IITPlaylist pl in itunes.LibrarySource.Playlists)
+            {
+
+                //Add playlist name to combo box
+                comboPlaylists.Items.Add(pl.Name);
+            }
+
+            //Load entire library playlist into variable
+            IITLibraryPlaylist mainLibrary = itunes.LibraryPlaylist;
+
+            //Display the entire library
+            GetTracks(mainLibrary);
+
+            //Tells the datagrid where to get data from
+            dataGridView1.DataSource = dataTable1;
+        }
+
+        private void GetTracks(IITPlaylist playlist)
+        {
+            long totalfilesize = 0;
+            dataTable1.Rows.Clear();
+            IITTrackCollection tracks = playlist.Tracks;
+            int numTracks = tracks.Count;
+            for (int currTrackIndex = 1; currTrackIndex <= numTracks; currTrackIndex++)
+            {
+                DataRow drnew = dataTable1.NewRow();
+                IITTrack currTrack = tracks[currTrackIndex];
+                drnew["artist"] = currTrack.Artist;
+                drnew["song name"] = currTrack.Name;
+                drnew["album"] = currTrack.Album;
+                drnew["genre"] = currTrack.Genre;
+
+
+                if (currTrack.Kind == ITTrackKind.ITTrackKindFile)
+                {
+                    IITFileOrCDTrack file = (IITFileOrCDTrack)currTrack;
+                    if (file.Location != null)
+                    {
+                        FileInfo fi = new FileInfo(file.Location);
+                        if (fi.Exists)
+                        {
+                            drnew["FileLocation"] = file.Location;
+                            totalfilesize += fi.Length;
+                        }
+                        else
+                            drnew["FileLocation"] = "not found " + file.Location;
+                    }
+                }
+
+                dataTable1.Rows.Add(drnew);
+            }
+            //lbl_numsongs.Text = "number of songs: " + dataTable1.Rows.Count.ToString() + ", total file size: " + (totalfilesize / 1024.00 / 1024.00).ToString("#,### mb");
+        }
+
+        private void comboPlaylists_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string playlist = comboPlaylists.SelectedItem.ToString();
+            foreach (IITPlaylist pl in itunes.LibrarySource.Playlists)
+            {
+                if (pl.Name == playlist)
+                {
+                    GetTracks(pl);
+                    break;
+                }
+            }
         }
     }
 }
